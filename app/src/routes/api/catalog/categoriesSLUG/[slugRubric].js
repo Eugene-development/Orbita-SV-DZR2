4a5155ff46014ae7016f7779e7aad0ff952e5c84
indexpro24@gmail.com <indexpro24@gmail.com>
2022-06-19 18:34:00 +0300

    git init

---
+++ app/src/routes/api/catalog/categoriesSLUG/[slugRubric].js
@@ -0,0 +1,21 @@
+import pkg from 'lodash';
+const { forEach, find } = pkg;
+import axios from "axios";
+export const get = async ({ params }) => {
+  const { slugRubric } = params;
+  const headers = {
+    Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`
+  };
+  const domain = import.meta.env.VITE_API_CRUD;
+  const urlRubric = `${domain}/get-all-rubric/`;
+  const rubrics = await axios(urlRubric, { headers });
+  let id;
+  forEach(rubrics.data, async function(value) {
+    let obj = find(value,{ "slug": slugRubric });
+    return id = obj.id
+  });
+  const url = `${domain}/get-rubric/${id}`;
+  const res = await fetch(url, { headers });
+  const categories = await res.json();
+  return { body: { categories } };
+};
