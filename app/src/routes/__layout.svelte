44fdc60c88048efe736aeab039549d7c9f3079b5
indexpro24@gmail.com <indexpro24@gmail.com>
2022-06-19 16:01:28 +0300

    git init

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -9,6 +9,15 @@
 	import '../app.css';
 	import Header from '$layout/header/v_001/index.svelte';
 	import Footer from '$layout/footer/v_001/index.svelte';
+
+
+	import { browser} from "$app/env";
+
+	import { v4 as uuid } from '@lukeed/uuid';
+	if (browser && localStorage.getItem('dataS') === null){
+		browser && localStorage.setItem('dataS', uuid())
+	}
+
 </script>
 
 <Header />
