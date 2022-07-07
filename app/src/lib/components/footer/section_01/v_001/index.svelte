068636896a85a24a23703d2c9537b6de39a4e8b2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-07-07 21:35:58 +0300

    git init

--- app/src/lib/components/footer/section_01/v_001/index.svelte
+++ app/src/lib/components/footer/section_01/v_001/index.svelte
@@ -54,14 +54,32 @@
 							<h3 class="text-lg font-semibold uppercase tracking-wider text-gray-400">
 								Контакты:
 							</h3>
-							<div class="mt-4 space-y-4">
+							<div class="mt-4 space-y-3">
 								<p class="text-base text-gray-100 hover:text-white">
 									г. Дзержинск, пр. Ленина, д. 121-В
 								</p>
+								<div class="flex text-base text-gray-100 hover:text-white">
+									<svg
+											class="my-auto mr-2 h-5 w-5 text-red-800 hidden md:block"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+												d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+										/>
+									</svg>
+									8 (8313) 31-02-28
+								</div>
+
 								<p class="text-base text-gray-100 hover:text-white">
 									г. Дзержинск, ул. Космонавтов, 4-Б
 								</p>
-								<p class="flex text-base text-gray-100 hover:text-white">
+								<div class="flex text-base text-gray-100 hover:text-white">
 									<svg
 										class="my-auto mr-2 h-5 w-5 text-red-800 hidden md:block"
 										fill="none"
@@ -77,46 +95,46 @@
 										/>
 									</svg>
 									8 (902) 303-36-54
-								</p>
-								<p class="flex text-base text-gray-100 hover:text-white">
-									<svg
-										class="my-auto mr-2 h-5 w-5 text-red-800 hidden md:block"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-										/>
-									</svg>
-									info@orbita-stroy.com
-								</p>
-								<a
-									class="flex text-base text-gray-100 hover:text-white"
-									href="https://vk.com/orbita_stroy"
-									target="_blank"
-								>
-									<svg
-										class="my-auto mr-2 h-5 w-5 text-red-800 hidden md:block"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-										/>
-									</svg>
-									vk.com/orbita_stroy
-								</a>
+								</div>
+<!--								<p class="flex text-base text-gray-100 hover:text-white">-->
+<!--									<svg-->
+<!--										class="my-auto mr-2 h-5 w-5 text-red-800 hidden md:block"-->
+<!--										xmlns="http://www.w3.org/2000/svg"-->
+<!--										fill="none"-->
+<!--										viewBox="0 0 24 24"-->
+<!--										stroke="currentColor"-->
+<!--										aria-hidden="true"-->
+<!--									>-->
+<!--										<path-->
+<!--											stroke-linecap="round"-->
+<!--											stroke-linejoin="round"-->
+<!--											stroke-width="2"-->
+<!--											d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"-->
+<!--										/>-->
+<!--									</svg>-->
+<!--									info@orbita-stroy.com-->
+<!--								</p>-->
+<!--								<a-->
+<!--									class="flex text-base text-gray-100 hover:text-white"-->
+<!--									href="https://vk.com/orbita_stroy"-->
+<!--									target="_blank"-->
+<!--								>-->
+<!--									<svg-->
+<!--										class="my-auto mr-2 h-5 w-5 text-red-800 hidden md:block"-->
+<!--										xmlns="http://www.w3.org/2000/svg"-->
+<!--										fill="none"-->
+<!--										viewBox="0 0 24 24"-->
+<!--										stroke="currentColor"-->
+<!--									>-->
+<!--										<path-->
+<!--											stroke-linecap="round"-->
+<!--											stroke-linejoin="round"-->
+<!--											stroke-width="2"-->
+<!--											d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"-->
+<!--										/>-->
+<!--									</svg>-->
+<!--									vk.com/orbita_stroy-->
+<!--								</a>-->
 							</div>
 						</div>
 					</div>
